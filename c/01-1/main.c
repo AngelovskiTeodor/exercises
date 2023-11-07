9e50f218818328415977690172e2a92959e3d4a3
Teodor Angelovski <angelovski.teo@outlook.com>
2023-11-07 10:04:58 +0100

    Finished exercise c/01-1

--- c/01-1/main.c
+++ c/01-1/main.c
@@ -2,6 +2,21 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    int vrednost;
+    scanf("%d", &vrednost);
+
+    int x = 5;
+    int y = 2;
+    int product = 5*2;
+    printf("Proizvodot pomegju %d i %d e %d \n",x,y,product);
+
+    int prvBroj = 25;
+    int vtorBroj = 18;
+    int sumaBroevi;
+    sumaBroevi = prvBroj+vtorBroj;
+
+    int nekojBroj;
+    scanf("%d", &nekojBroj);
+
     return 0;
 }
