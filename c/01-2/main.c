4529c24f664f8cee18018e9d264a3fa6d8bc924a
Teodor Angelovski <angelovski.teo@outlook.com>
2023-11-07 10:37:18 +0100

    Finished exercise c/01-2

---
+++ c/01-2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, sum;
+    scanf_s("%d", &a);
+    scanf_s("%d", &b);
+    scanf_s("%d", &c);
+    scanf_s("%d", &d);
+    sum = a + b + c + d;
+    printf("%d",sum);
+    getchar();
+    return 0;
+}
